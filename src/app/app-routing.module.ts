d16d7f929ca8574b50e60f3101eb8302ab53f123
Ihar Dubadzelau <ihar@dubadzelau.xyz>
2020-04-27 00:47:24 -0400

    1. Creating a routing structure. Layouts pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,8 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {Routes, RouterModule, PreloadAllModules} from '@angular/router';
 import {MainLayoutComponent} from './shared/components/main-layout/main-layout.component';
 import {HomePageComponent} from './home-page/home-page.component';
 import {PostPageComponent} from './post-page/post-page.component';
-
 
 const routes: Routes = [
   {
@@ -12,11 +11,16 @@ const routes: Routes = [
       {path: '', component: HomePageComponent},
       {path: 'post/:id', component: PostPageComponent}
     ]
+  },
+  {
+    path: 'admin', loadChildren: () => import('./admin/admin.module').then( m => m.AdminModule)
   }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    preloadingStrategy: PreloadAllModules
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
