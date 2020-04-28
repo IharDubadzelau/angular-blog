e743818b7c08c9483abd83ae42669a9be2e7c135
Ihar Dubadzelau <ihar@dubadzelau.xyz>
2020-04-28 00:10:24 -0400

    3. Done authorization. And support DB Firebase. Add page - create post.

--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {ActivatedRoute, Params, Router} from '@angular/router';
+
 import {User} from '../../shared/interfaces';
+import {AuthService} from '../shared/services/auth.service';
+
 
 @Component({
   selector: 'app-login-page',
@@ -10,10 +14,24 @@ import {User} from '../../shared/interfaces';
 export class LoginPageComponent implements OnInit {
 
   form: FormGroup;
+  submitted = false;
+  msg: string;
 
-  constructor() { }
+  constructor(
+    public auth: AuthService,
+    private router: Router,
+    private aroute: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.aroute.queryParams.subscribe( (params: Params) => {
+      if ( params.loginAgain ) {
+        this.msg = 'Пожалуйста введите данные.';
+      } else if ( params.authFailed ) {
+        this.msg = 'Сессия истекла. Введите данные заново.';
+      }
+    });
+
     this.form = new FormGroup( {
       email: new FormControl(null, [
         Validators.required,
@@ -31,9 +49,20 @@ export class LoginPageComponent implements OnInit {
       return;
     }
 
+    this.submitted = true;
+
     const user: User = {
       email: this.form.value.email,
-      password: this.form.value.password
+      password: this.form.value.password,
+      returnSecureToken: false
     };
+
+    this.auth.login(user).subscribe( () => {
+      this.form.reset();
+      this.router.navigate(['/admin', 'dashboard']);
+      this.submitted = false;
+    }, () => {
+      this.submitted = false;
+    });
   }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
-export const environment = {
-  production: true
+import {Environment} from './interface';
+
+export const environment: Environment = {
+  production: true,
+  apiKey: 'AIzaSyCyuACx258fypEoNBuE_XqKMGbgqpuEGEM',
+  fbDbUrl: 'https://angular-blog-snik.firebaseio.com'
 };
