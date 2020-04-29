35610abe55fb6c7e604620ff4b9cbfae57ecd291
Ihar Dubadzelau <ihar@dubadzelau.xyz>
2020-04-29 12:28:10 -0400

    4. Completed work with adding and updating posts in the admin panel. Added alerts.

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -8,13 +8,22 @@ import { LoginPageComponent } from './login-page/login-page.component';
 import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
 import { CreatePageComponent } from './create-page/create-page.component';
 import { EditPageComponent } from './edit-page/edit-page.component';
-import {AuthService} from './shared/services/auth.service';
 import {SharedModule} from '../shared/shared.module';
 import {AuthGuard} from './shared/services/auth.guard';
+import {SearchPipe} from './shared/search.pipe';
+import { AlertComponent } from './shared/components/alert/alert.component';
+import {AlertService} from './shared/services/alert.service';
 
 
 @NgModule({
-  declarations: [AdminLayoutComponent, LoginPageComponent, DashboardPageComponent, CreatePageComponent, EditPageComponent],
+  declarations: [
+    AdminLayoutComponent,
+    LoginPageComponent,
+    DashboardPageComponent,
+    CreatePageComponent,
+    EditPageComponent,
+    SearchPipe,
+    AlertComponent],
   imports: [
     CommonModule,
     FormsModule,
@@ -35,7 +44,10 @@ import {AuthGuard} from './shared/services/auth.guard';
   exports: [
     RouterModule
   ],
-  providers: [AuthGuard]
+  providers: [
+    AuthGuard,
+    AlertService
+  ]
 })
 export class AdminModule {
 
